0c53d529f615c3bd1fb2aae252aca0048dc7c5d5
celestedevelopment <mvizzim@gmail.com>
2025-10-21 17:03:33 +0200

    sistemate freccie

--- crypto-checkout/app/page.tsx
+++ crypto-checkout/app/page.tsx
@@ -623,32 +623,20 @@ export default function Home() {
                           </div>
                         </div>
                         <div className="flex items-center gap-2">
-                          {/* Checkmark when selected */}
-                          <svg 
-                            width="16" 
-                            height="16" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="text-black"
-                          >
-                            <path 
-                              d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z" 
-                              fill="currentColor"
-                            />
-                          </svg>
                           {/* Dropdown Arrow */}
                           <svg 
-                            width="16" 
-                            height="16" 
-                            viewBox="0 0 24 24" 
+                            width="15" 
+                            height="15" 
+                            viewBox="0 0 15 15" 
                             fill="none" 
                             xmlns="http://www.w3.org/2000/svg"
-                            className={`transition-transform ${isWalletDropdownOpen ? 'rotate-180' : ''}`}
+                            className={`ml-2 h-4 w-4 shrink-0 text-black transition-transform duration-200 ${isWalletDropdownOpen ? 'rotate-180' : ''}`}
                           >
                             <path 
-                              d="M7 10l5 5 5-5z" 
-                              fill="currentColor"
+                              d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z" 
+                              fill="currentColor" 
+                              fillRule="evenodd" 
+                              clipRule="evenodd"
                             />
                           </svg>
                         </div>
